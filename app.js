3b69cd974422ae672be0f8a4613afeef52913d48
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-22 00:48:48 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+var Favorite_fruits = ["Apple", "Mango", "Graphes"];
+if (Favorite_fruits.includes('Apple')) {
+    console.log('I Really like Apple');
+}
+if (Favorite_fruits.includes('Orange')) {
+    console.log('I Really like Orange');
+}
+if (Favorite_fruits.includes('Graphes')) {
+    console.log('I Really like Graphes');
+}
+if (Favorite_fruits.includes('Mango')) {
+    console.log('I Really like Mango');
+}
+if (Favorite_fruits.includes('Bananas')) {
+    console.log('I Really like Bananas');
+}
---
+++ app.ts
@@ -0,0 +1,22 @@
+// let Favorite_fruits : string [] = ["Apple","Mango","Graphes"]
+
+// if(Favorite_fruits.includes('Apple')){
+//     console.log ('I Really like Apple')
+// }
+
+// if(Favorite_fruits.includes('Orange')){
+//     console.log ('I Really like Orange')
+// }
+
+
+// if(Favorite_fruits.includes('Graphes')){
+//     console.log ('I Really like Graphes')
+// }
+
+// if(Favorite_fruits.includes('Mango')){
+//     console.log ('I Really like Mango')
+// }
+
+// if(Favorite_fruits.includes('Bananas')){
+//     console.log ('I Really like Bananas')
+// }
